9c96138714cfc4fe8c605f16d0d8f58544984072
Melanie Daoheuang <daoh0031@gmail.com>
2021-09-16 13:50:32 -0600

    Add clear button

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -3,29 +3,33 @@ import './Buttons.css';
 function Buttons() {
   return (
     <section className='button-container'>
-      <section className='row'>
+      <section>
         <button className='main-button'>7</button>
         <button className='main-button'>8</button>
         <button className='main-button'>9</button>
-        <button className='main-button'>/</button>
+        <button className='main-button operator'>/</button>
       </section>
       <section className='row'>
         <button className='main-button'>4</button>
         <button className='main-button'>5</button>
         <button className='main-button'>6</button>
-        <button className='main-button'>X</button>
+        <button className='main-button operator'>x</button>
       </section>
-      <section className='row'>
+      <section>
         <button className='main-button'>1</button>
         <button className='main-button'>2</button>
         <button className='main-button'>3</button>
-        <button className='main-button'>+</button>
+        <button className='main-button operator'>+</button>
       </section>
-      <section className='row'>
-        <button className='main-button'>.</button>
+      <section>
         <button className='main-button'>0</button>
-        <button className='main-button'>=</button>
-        <button className='main-button'>-</button>
+        <button className='main-button'>.</button>
+        <button className='main-button'>+/-</button>
+        <button className='main-button operator'>-</button>
+      </section>
+      <section>
+        <button className='main-button clear'>Clear</button>
+        <button className='main-button operator'>=</button>
       </section>
     </section>
   )
