1d7690bff28f441eb5bfc51ab124685f23576bc5
Melanie Daoheuang <daoh0031@gmail.com>
2021-09-16 14:30:42 -0600

    Add handleClick method

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -1,37 +1,49 @@
+import { useState } from 'react';
 import './Buttons.css';
 
 function Buttons() {
+  const [result, setResult] = useState('');
+
+  const handleClick = (event) => {
+    setResult(event.target.name)
+  }
+
   return (
-    <section className='button-container'>
-      <section>
-        <button className='main-button'>7</button>
-        <button className='main-button'>8</button>
-        <button className='main-button'>9</button>
-        <button className='main-button operator'>/</button>
+    <main>
+      <form>
+        <input type='text' value={result}/>
+      </form>
+      <section className='button-container'>
+        <section>
+          <button onClick={handleClick} name='7' className='main-button'>7</button>
+          <button onClick={handleClick} name='8' className='main-button'>8</button>
+          <button onClick={handleClick} name='9' className='main-button'>9</button>
+          <button onClick={handleClick} name='/' className='main-button operator'>&divide;</button>
+        </section>
+        <section>
+          <button onClick={handleClick} name='4' className='main-button'>4</button>
+          <button onClick={handleClick} name='5' className='main-button'>5</button>
+          <button onClick={handleClick} name='6' className='main-button'>6</button>
+          <button onClick={handleClick} name='*' className='main-button operator'>&times;</button>
+        </section>
+        <section>
+          <button onClick={handleClick} name='1' className='main-button'>1</button>
+          <button onClick={handleClick} name='2' className='main-button'>2</button>
+          <button onClick={handleClick} name='3' className='main-button'>3</button>
+          <button onClick={handleClick} name='+' className='main-button operator'>+</button>
+        </section>
+        <section>
+          <button onClick={handleClick} name='0' className='main-button'>0</button>
+          <button onClick={handleClick} name='.' className='main-button'>.</button>
+          <button onClick={handleClick} name='-' className='main-button'>+/-</button>
+          <button onClick={handleClick} name='-' className='main-button operator'>&ndash;</button>
+        </section>
+        <section>
+          <button onClick={handleClick} className='main-button clear'>Clear</button>
+          <button onClick={handleClick} name='=' className='main-button operator'>=</button>
+        </section>
       </section>
-      <section className='row'>
-        <button className='main-button'>4</button>
-        <button className='main-button'>5</button>
-        <button className='main-button'>6</button>
-        <button className='main-button operator'>x</button>
-      </section>
-      <section>
-        <button className='main-button'>1</button>
-        <button className='main-button'>2</button>
-        <button className='main-button'>3</button>
-        <button className='main-button operator'>+</button>
-      </section>
-      <section>
-        <button className='main-button'>0</button>
-        <button className='main-button'>.</button>
-        <button className='main-button'>+/-</button>
-        <button className='main-button operator'>-</button>
-      </section>
-      <section>
-        <button className='main-button clear'>Clear</button>
-        <button className='main-button operator'>=</button>
-      </section>
-    </section>
+    </main>
   )
 }
 
