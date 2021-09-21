ad094201049fe197f124b01aa419b291f4bdbe4d
Melanie Daoheuang <daoh0031@gmail.com>
2021-09-20 20:53:14 -0600

    Add method to calculate another operator and number

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -30,18 +30,17 @@ function Buttons() {
   });
 
   // Toggle last integer to be positive or negative 
-  const toggleNegativePostivie = () => {
-    let integer = Math.sign(data);
-    console.log(integer)
-    if (integer === 1) {
-      setData(-Math.abs(data));
-    } else if (integer === -1) {
-      setData(Math.abs(data));
-    }
-
-
-    console.log(data)
-  }
+  // const toggleNegativePostivie = () => {
+  //   let integer = Math.sign(splitString().pop())
+  //   console.log(integer)
+  //   if (integer === 1) {
+  //     console.log('pos')
+  //     setData(-Math.abs(integer));
+  //   } else if (integer === -1) {
+  //     console.log('neg')
+  //     setData(Math.abs(integer));
+  //   }
+  // }
 
   // split the string by spaces >> 1 + 2 + 3 >> [1 '+' 2] ['+' 3] >> [3 '+' 3]
   const splitString = () => {
@@ -53,26 +52,31 @@ function Buttons() {
       }
     });
     return numbers;
-    // part 2. consider another operator
   }
 
-  const calculateNumbers = () => {
-    let a, operatorString, b;
-    [a, operatorString, b] = splitString();
-
-    switch(operatorString) { 
-      case '+': setData(a + b);
-      break; 
-      case '-': setData(a - b);
-      break; 
-      case '÷': setData(a / b);
-      break; 
-      case 'x': setData(a * b); 
-      break;
+  const calculateNumbers = (a, operator, b) => {
+    switch(operator) { 
+      case '+': return a + b;
+      case '-': return a - b; 
+      case 'x': return a * b; 
+      case '÷': return a / b;
     } 
   }
 
+  // method to check operator & rearrange 
 
+
+  const setResult = () => {
+    let a, operator, b, secondOperator, c;
+    [a, operator, b, secondOperator, c] = splitString();
+
+    if (splitString().length > 3) {
+      let firstResult = calculateNumbers(a, operator, b)
+      setData(calculateNumbers(firstResult, secondOperator, c));
+    } else if (splitString().length === 3) {
+      setData(calculateNumbers(a, operator, b));
+    }
+  }
 
   return (
     <main>
@@ -87,8 +91,8 @@ function Buttons() {
       <section className='button-container'>
         {buttonList}
         <button onClick={handleClick} name='.' className='main-button'>.</button>
-        <button onClick={toggleNegativePostivie} className='main-button'>+/-</button>
-        <button onClick={calculateNumbers} name='=' className='main-button operator equals'>=</button>
+        <button onClick={handleClick} className='main-button'>+/-</button>
+        <button onClick={setResult} name='=' className='main-button operator equals'>=</button>
       </section>
     </main>
   )
