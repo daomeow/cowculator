1f6c3453714d31a85771a106a1f2685afe01a24d
Melanie Daoheuang <daoh0031@gmail.com>
2021-09-22 08:58:59 -0600

    Add tests to check operators

--- cypress/integration/calculator_spec.js
+++ cypress/integration/calculator_spec.js
@@ -12,12 +12,44 @@ describe('Calculator', () => {
       .get('button').eq(19).contains('+/-')
   });
 
-  it.only('should be able to divide', () => {
+  // Functionality to divide, multiply, add, and subtract 
+  it('should be able to divide whole numbers', () => {
     cy.get('button').eq(10).click()
       .get('button').eq(14).click()
       .get('button').eq(5).click()
       .get('button').eq(11).click()
       .get('button').eq(20).click()
-  })
+      .get('.display > :nth-child(1)').contains(5)
+  });
+
+  it('should be able to multiply three numbers', () => {
+    cy.get('button').eq(7).click()
+      .get('button').eq(9).click()
+      .get('button').eq(11).click()
+      .get('button').eq(9).click()
+      .get('button').eq(12).click()
+      .get('button').eq(20).click()
+      .get('.display > :nth-child(1)').contains(30)
+  });
+
+  it('should be able to add decimal numbers', () => {
+    cy.get('[name="9"]').click()
+      .get('[name="."]').click()
+      .get('[name="2"]').click()
+      .get('[name=" + "]').click()
+      .get('[name="5"]').click()
+      .get('[name="="]').click()
+      .get('.display > :nth-child(1)').contains(14.2)
+  });
+
+  it('should be able to calculate a subtraction problem', () => {
+    cy.get('[name="9"]').click()
+      .get('[name="1"]').click()
+      .get('[name=" - "]').click()
+      .get('[name="5"]').click()
+      .get('[name="5"]').click()
+      .get('[name="="]').click()
+      .get('.display > :nth-child(1)').contains(36)
+  });
 
 });
